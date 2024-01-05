7fddfa4a4077bc547015b98605204e8ae725b0a3
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2024-01-05 11:17:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = '8dcbd44ea791b97fae4c8a647b7b4aca'; // Replace with your OpenWeatherMap API key
+
+async function getWeather() {
+    const cityInput = document.getElementById('cityInput').value;
+
+    if (cityInput) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`);
+
+            if (!response.ok) {
+                throw new Error(`Failed to fetch weather data. Status: ${response.status}`);
+            }
+
+            const weatherData = await response.json();
+
+            displayWeather(weatherData);
+        } catch (error) {
+            console.error('Error fetching weather data:', error.message);
+        }
+    } else {
+        alert('Please enter a city name.');
+    }
+}
+
+function displayWeather(data) {
+    const cityNameElement = document.getElementById('cityName');
+    const temperatureElement = document.getElementById('temperature');
+    const descriptionElement = document.getElementById('description');
+
+    cityNameElement.textContent = data.name;
+    temperatureElement.textContent = `Temperature: ${data.main.temp} °C`;
+    descriptionElement.textContent = `Description: ${data.weather[0].description}`;
+}
+
